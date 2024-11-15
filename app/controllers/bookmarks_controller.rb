9e8792e11d737b77781996a3e75e1012e966a9ee
martino vivien <vivien.martino@hotmail.fr>
2024-11-15 13:57:11 +0100

    tout vert

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -9,7 +9,17 @@ class BookmarksController < ApplicationController
     @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(bookmark_params)
     @bookmark.list = @list
-    redirect_to lists_path if @bookmark.save!
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
   end
 
   private
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,21 +9,27 @@ class ListsController < ApplicationController
   end
 
   def new
-    @list = List.new()
+    @list = List.new
   end
 
   def create
     @list = List.new(list_params)
-    if @list.save!
-      redirect_to lists_path
+    if @list.save
+      redirect_to list_path(@list)
     else
       render :new, status: :unprocessable_entity
     end
   end
 
+  def destroy
+    @list = List.find(params[:id])
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
   private
 
   def list_params
-    params.require(:list).permit(:name, :created_at, :image_url)
+    params.require(:list).permit(:name, :image_url)
   end
 end
